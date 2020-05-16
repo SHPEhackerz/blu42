992e3c9f7ad45274734f9d3f1536d2662c453d05
V0R3T3X <65465691+V0RT3X-Web@users.noreply.github.com>
2020-05-16 17:45:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var http, director, bot, router, server, port;
+var http, director, cool, bot, router, server, port;
 
 http        = require('http');
 director    = require('director');
+cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
@@ -28,5 +29,5 @@ server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("sup");
+  this.res.end("Hey, I'm Cool Guy.");
 }
